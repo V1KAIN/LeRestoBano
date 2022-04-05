6b7c6e4e077c5c87a49cbffd73c4ab435cb4bdfe
Valou <sulfeur84@gmail.com>
2022-04-05 10:41:10 +0200

    Add Scene transition
    need some art

--- Assets/Valentin/Scripts/DoSomethingOnTrigger.cs
+++ Assets/Valentin/Scripts/DoSomethingOnTrigger.cs
@@ -3,11 +3,19 @@ using UnityEngine.SceneManagement;
 public class DoSomethingOnTrigger : MonoBehaviour
 {
     public string SceneName;
+
+    public Animator animator;
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneName);
+            animator.SetTrigger("go");
+            Invoke("Transition",0.5f);
         }
+    }
+
+    public void Transition()
+    {
+        SceneManager.LoadScene(SceneName);
     }
 }
