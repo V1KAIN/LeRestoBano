0fbf97d7b40bc123d59c8bfa1e05e3b4dd74ddb0
VIKAIN <thomas.hebert2020@gmail.com>
2022-04-05 12:47:18 +0200

    Finished Dialogue Management

--- Assets/Thomas(H)/Scripts/DialogueManager.cs
+++ Assets/Thomas(H)/Scripts/DialogueManager.cs
@@ -13,18 +13,29 @@ public class DialogueManager : MonoBehaviour
     public TextMeshProUGUI actorName;
     public TextMeshProUGUI messageText;
     public RectTransform backgroundBox;
+    [SerializeField] private GameObject DialogueContentHolder;
 
     [Header("Parameters")]
     [SerializeField] KeyCode NextMessageKey = KeyCode.Space;
     [SerializeField] KeyCode PreviousMessageKey = KeyCode.Backspace;
     [SerializeField] private bool CanGoBackMessages = true;
+    [SerializeField] private float typingSpeed = 0.04f;
+    
+    
         
     //Privates Fields    
     private Dialogue[] _currentMessages;
     private Actor[] _currentActors;
     private int _activeMessage = 0;
+    private Animator DialogueBoxAnimator;
 
     private bool _isInDialogue = false;
+
+    private void Start()
+    {
+        DialogueBoxAnimator = gameObject.GetComponent<Animator>();
+        DesactivateDCH();
+    }
 
     private void Update()
     {
@@ -39,6 +50,7 @@ public class DialogueManager : MonoBehaviour
         _currentActors = actors;
         _activeMessage = 0;
         _isInDialogue = true;
+        DialogueContentHolder.SetActive(true);
         DisplayDialogue();
         Debug.Log("Started conversation, Loaded " + messages.Length + " messages between " + actors.Length + " actors" );
     }
@@ -58,20 +70,25 @@ public class DialogueManager : MonoBehaviour
             { 
                 Debug.Log("Conversation Ended!");
                 _isInDialogue = false;
+                DialogueBoxAnimator.SetBool("GoOut", true);
+                Invoke(nameof(DesactivateDCH), .20f); 
             }
         }
     }
     
     public void PreviousDialogue()
     {
-        //Regarde si le joueur est dans un dialogue 
-        if(_isInDialogue){
-            //Si le nombre de message est supérieur a 0 il revient au message précédent
-            if (_activeMessage > 0)
-            {
-                _activeMessage--;
-                DisplayDialogue();
-            } 
+        if (CanGoBackMessages)
+        {
+            //Regarde si le joueur est dans un dialogue 
+            if(_isInDialogue){
+                //Si le nombre de message est supérieur a 0 il revient au message précédent
+                if (_activeMessage > 0)
+                {
+                    _activeMessage--;
+                    DisplayDialogue();
+                } 
+            }
         }
     }
     
@@ -79,10 +96,33 @@ public class DialogueManager : MonoBehaviour
     void DisplayDialogue()
     {
         Dialogue dialogueToDisplay = _currentMessages[_activeMessage];
-        messageText.text = dialogueToDisplay.Message;
+        //messageText.text = dialogueToDisplay.Message;
+        if(displayLineCoroutine != null) StopCoroutine(displayLineCoroutine);      
+        displayLineCoroutine = StartCoroutine(DisplayLine(dialogueToDisplay.Message));
 
         Actor actorToDisplay = _currentActors[dialogueToDisplay.ActorId];
         actorName.text = actorToDisplay.Name;
         actorImage.sprite = actorToDisplay.Sprite;
+    }
+
+
+    private Coroutine displayLineCoroutine;
+    private IEnumerator DisplayLine(string line)
+    {
+        //enleve le dialogue précédent
+        messageText.text = "";
+        
+        //Affiche un lettre a la fois
+        foreach (char letter in line.ToCharArray())
+        {
+            messageText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    void DesactivateDCH()
+    {
+        DialogueContentHolder.SetActive(false);
+        DialogueBoxAnimator.SetBool("GoOut",false);
     }
 }
