6185e26de177130e1b23f334af44459abd309c5f
Valou <sulfeur84@gmail.com>
2022-04-07 15:20:41 +0200

    Audio

--- Assets/Valentin/Scripts/Object.cs
+++ Assets/Valentin/Scripts/Object.cs
@@ -22,6 +22,7 @@ public class Object : MonoBehaviour
     {
         if (other.CompareTag("Pied") && Poulet)
         {
+            FindObjectOfType<AudioManager>().PitchAlea("Plus");
             GetComponent<DialogueTrigger>().StartDialogue();
             InventoryScript.Poulet += 1;
             other.GetComponentInParent<PopUpTextScript>().SpawnText("poulet");
@@ -29,6 +30,7 @@ public class Object : MonoBehaviour
         }
         if (other.CompareTag("Pied") && Bacon)
         {
+            FindObjectOfType<AudioManager>().PitchAlea("Plus");
             GetComponent<DialogueTrigger>().StartDialogue();
             InventoryScript.Bacon += 1;
             other.GetComponentInParent<PopUpTextScript>().SpawnText("bacon");
@@ -36,30 +38,35 @@ public class Object : MonoBehaviour
         }
         if (other.CompareTag("Main") && Frite)
         {
+            FindObjectOfType<AudioManager>().PitchAlea("Plus");
             InventoryScript.Frite += 1;
             other.GetComponentInParent<PopUpTextScript>().SpawnText("frite");
             Destroy(gameObject);
         }
         if (other.CompareTag("Main") && Pain)
         {
+            FindObjectOfType<AudioManager>().PitchAlea("Plus");
             InventoryScript.Pain += 1;
             other.GetComponentInParent<PopUpTextScript>().SpawnText("pain");
             Destroy(gameObject);
         }
         if (other.CompareTag("Main") && Galette)
         {
+            FindObjectOfType<AudioManager>().PitchAlea("Plus");
             InventoryScript.Galette += 1;
             other.GetComponentInParent<PopUpTextScript>().SpawnText("galette");
             Destroy(gameObject);
         }
         if (other.CompareTag("Torse") && Mayo)
         {
+            FindObjectOfType<AudioManager>().PitchAlea("Plus");
             InventoryScript.Mayo += 1;
             other.GetComponentInParent<PopUpTextScript>().SpawnText("mayo");
             Destroy(gameObject);
         }
         if (other.CompareTag("Torse") && Ketchup)
         {
+            FindObjectOfType<AudioManager>().PitchAlea("Plus");
             InventoryScript.Ketchup += 1;
             other.GetComponentInParent<PopUpTextScript>().SpawnText("ketchup");
             Destroy(gameObject);
@@ -67,30 +74,35 @@ public class Object : MonoBehaviour
         
         if (other.CompareTag("Torse") && Burger)
         {
+            FindObjectOfType<AudioManager>().PitchAlea("Plus");
             InventoryScript.Burger = true;
             other.GetComponentInParent<PopUpTextScript>().SpawnText("burger");
             Destroy(gameObject);
         }
         if (other.CompareTag("Torse") && OctaB)
         {
+            FindObjectOfType<AudioManager>().PitchAlea("Plus");
             InventoryScript.OctaB = true;
             other.GetComponentInParent<PopUpTextScript>().SpawnText("menu octomachin");
             Destroy(gameObject);
         }
         if (other.CompareTag("Torse") && NonBurger)
         {
+            FindObjectOfType<AudioManager>().PitchAlea("Plus");
             InventoryScript.NonBurger = true;
             other.GetComponentInParent<PopUpTextScript>().SpawnText("non burger");
             Destroy(gameObject);
         }
         if (other.CompareTag("Torse") && Wrap)
         {
+            FindObjectOfType<AudioManager>().PitchAlea("Plus");
             InventoryScript.Wrap = true;
             other.GetComponentInParent<PopUpTextScript>().SpawnText("wrap");
             Destroy(gameObject);
         }
         if (other.CompareTag("Torse") && CheeseBurger)
         {
+            FindObjectOfType<AudioManager>().PitchAlea("Plus");
             InventoryScript.CheeseBurger = true;
             other.GetComponentInParent<PopUpTextScript>().SpawnText("cheese burger");
             Destroy(gameObject);
@@ -100,9 +112,11 @@ public class Object : MonoBehaviour
     public void AddFromage()
     {
         InventoryScript.Fromage += 1;
+        FindObjectOfType<AudioManager>().PitchAlea("Plus");
     }
     public void AddSteak()
     {
         InventoryScript.Steak += 1;
+        FindObjectOfType<AudioManager>().PitchAlea("Plus");
     }
 }
